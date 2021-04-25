e1b6e5890a1ef91848f79d6be88fc309d1ddacf5
theCapm <rmattgrimsley@gmail.com>
2021-04-24 22:28:31 -0500

    starting over
    Starting over with a fresh install and using the 'project-template' download from the course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
-import { Component, NgModule } from '@angular/core';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  name = 'matt';
 }
