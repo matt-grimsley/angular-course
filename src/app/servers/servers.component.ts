6fb92551f41019c54d3459b89f88d99ab649ba27
theCapm <rmattgrimsley@gmail.com>
2021-04-25 23:35:38 -0500

    Finished Databinding section
    Completed through the Databinding assignment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,33 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
+  // selector: "app-servers",
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
   selector: "app-servers",
-  template: ` <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: "./servers.component.html",
   styleUrls: ["./servers.component.css"],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  serverName = "TestServer";
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      "Server was created! Name is " + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
