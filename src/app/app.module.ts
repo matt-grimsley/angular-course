1e644cd3288bb3dfef1b652ef3bc9e523e0d0182
theCapm <rmattgrimsley@gmail.com>
2021-04-26 23:37:52 -0500

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,9 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
 import { ServersComponent } from "./servers/servers.component";
-import { AssignmentsComponent } from "./assignments/assignments.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    AssignmentsComponent,
-  ],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
